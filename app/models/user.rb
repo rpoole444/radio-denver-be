4a0958c1e5856c59f87a983f01a2194e38514415
Nathan Lambertson <lambertones@gmail.com>
2024-04-25 12:24:19 -0500

    Add basic user table with model and test

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+end
---
+++ spec/factories/first_names.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :first_name do
+    last_name { "MyString" }
+    password { "MyString" }
+    email { "MyString" }
+  end
+end
