0964be8727cb6fadc0786c30bfe2704088db3aaf
Nathan Lambertson <lambertones@gmail.com>
2024-04-25 12:35:04 -0500

    Add test for unique email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,27 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
   end
+
+  describe 'users' do
+    it "should be able to create a new user" do
+      user1 = User.create!(first_name: "John", last_name: "Doe", email: "lame@gmail.com", password: "1234password")
+      user2 = User.create!(first_name: "Jane", last_name: "Doe", email: "lamby@gmail.com", password: "1265password")
+      user3 = User.create!(first_name: "Bill", last_name: "Dot", email: "larre@gmail.com", password: "12345pass")
+
+      expect(user1.first_name).to eq("John")
+      expect(user1.last_name).to eq("Doe")
+      expect(user1.email).to eq("lame@gmail.com")
+      expect(user1.password).to be_a(String)
+      expect(User.count).to eq(3)
+    end
+
+    it "cannot create a user with an existing email" do
+      user1 = User.create!(first_name: "John", last_name: "Doe", email: "lame@gmail.com", password: "1234password")
+      user2 = User.new(first_name: "Jane", last_name: "Doe", email: "lame@gmail.com", password: "1265password")
+
+      expect { user2.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      expect(user2.save).to eq(false)
+      expect(User.count).to eq(1)
+    end
+  end
 end
