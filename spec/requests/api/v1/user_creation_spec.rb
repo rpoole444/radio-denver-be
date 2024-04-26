8f494748155d8450bb6bc25c1b3384cfaed7a54a
Nathan Lambertson <lambertones@gmail.com>
2024-04-26 09:24:55 -0500

    Add test for simple error handling of blank fields for user creation

--- spec/requests/api/v1/user_creation_spec.rb
+++ spec/requests/api/v1/user_creation_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.describe "create a new user endpoint", type: :request do
   describe "POST /api/v1/users" do
     it "creates a new user in the database" do
-      headers = { "CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json" }
       post "/api/v1/users", params: {
         user: {
           first_name: "Jerry",
@@ -18,6 +17,23 @@ RSpec.describe "create a new user endpoint", type: :request do
 
       expect(response.status).to eq(201)
       expect(json_response["data"]["attributes"]["first_name"]).to eq("Jerry")
+    end
+
+    it "sends back a 422 if the user is not created" do
+      post "/api/v1/users", params: {
+        user: {
+          first_name: "",
+          last_name: "",
+          email: "",
+          password: "",
+          password_confirmation: ""
+        }
+      }.to_json, headers: { "Content-Type" => "application/json", "Accept" => "application/json" }
+
+      json_response = JSON.parse(response.body)
+
+      expect(response.status).to eq(422)
+      expect(json_response["errors"]).to eq(["Password can't be blank", "First name can't be blank", "Last name can't be blank", "Email can't be blank", "Password digest can't be blank"])
     end
   end
 end
