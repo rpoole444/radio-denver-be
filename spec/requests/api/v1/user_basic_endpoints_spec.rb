243c20e8baee8166a376a2ba2ee93649bb5566c6
Nathan Lambertson <lambertones@gmail.com>
2024-04-26 16:18:37 -0500

    Add user show and index endpoints

---
+++ spec/requests/api/v1/user_basic_endpoints_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "regular old user endpoints", type: :request do
+  describe "GET /api/v1/users" do
+    it "retuns an index of all users" do
+      user1 = User.create!(first_name: "John", last_name: "Doe", email: "lame@gmail.com", password: "1234password", password_confirmation: "1234password")
+      user2 = User.create!(first_name: "Jane", last_name: "Doe", email: "lamby@gmail.com", password: "1265password", password_confirmation: "1265password")
+      user3 = User.create!(first_name: "Bill", last_name: "Dot", email: "larre@gmail.com", password: "12345pass", password_confirmation: "12345pass")
+
+      get "/api/v1/users", headers: { "Content-Type" => "application/json", "Accept" => "application/json" }
+
+      json_response = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:success)
+      expect(json_response["data"].length).to eq(3)
+      expect(json_response["data"][0]["attributes"]["first_name"]).to eq("John")
+    end
+  end
+
+  describe "GET /api/v1/users/:id" do
+    it "returns a single user" do
+      user1 = User.create!(first_name: "John", last_name: "Doe", email: "lame@gmail.com", password: "1234password", password_confirmation: "1234password")
+
+      get "/api/v1/users/#{user1.id}", headers: { "Content-Type" => "application/json", "Accept" => "application/json" }
+
+      json_response = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:success)
+      expect(json_response["data"]["attributes"]["first_name"]).to eq("John")
+    end
+  end
+end
