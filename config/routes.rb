19cd987b6251083370cd654d3a0c314684b3ab2f
Nathan Lambertson <lambertones@gmail.com>
2024-04-27 14:34:43 -0500

    Create session controller and tests

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create]
+      resources :sessions, only: [:create]
     end
   end
 end
