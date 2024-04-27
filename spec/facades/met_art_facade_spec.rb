fb11a15e47fd45c754320e706f4aa16ea7fe40f1
Nathan Lambertson <lambertones@gmail.com>
2024-04-26 20:05:38 -0500

    √

--- spec/facades/met_art_facade_spec.rb
+++ spec/facades/met_art_facade_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe MetArtFacade, vcr: true, type: :facade do
       art = MetArtFacade.new.art_single(34545)
 
       expect(art).to be_a(MetArtPoro)
+      expect(art.title).to be_a(String)
+      expect(art.image_url).to be_a(String)
     end
   end
 end
