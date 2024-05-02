be2c3b0b80cf4083a9196570a5ddd34e7fd5519a
Nathan Lambertson <lambertones@gmail.com>
2024-05-02 10:30:38 -0500

    Adapt password reset controller logic to API structure

--- app/controllers/api/v1/password_resets_controller.rb
+++ app/controllers/api/v1/password_resets_controller.rb
@@ -1,29 +1,27 @@
-class PasswordResetsController < ApplicationController
-  def new
-  end
+class Api::V1::PasswordResetsController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def create
-    @user = User.find_by(email: params[:email])
-    if @user
-      @user.generate_password_token!
-      UserMailer.reset_password_email(@user).deliver_later
-      flash[:notice] = "Email sent with password reset instructions."
+    user = User.find_by(email: params[:email])
+    if user
+      user.generate_password_token!
+      UserMailer.reset_password_email(user).deliver_later
+      render json: { message: "Email sent with password reset instructions" }, status: :ok
     else
-      flash.now[:alert] = "Email address not found."
-      render :new
+      render json: { error: "Email address not found" }, status: :not_found
     end
   end
 
-  def edit
-    @user = User.find_by(reset_password_token: params[:token], email: params[:email])
-  end
-
   def update
-    @user = User.find_by(reset_password_token: params[:token], email: params[:email])
-    if @user.reset_password!(params[:user][:password], params[:user][:password_confirmation])
-      flash[:notice] = "Password has been reset."
+    user = User.find_by(reset_password_token: params[:token], email: params[:email])
+    if user.present? && user.password_token_valid?
+      if user.reset_password!(params[:password], params[:password_confirmation])
+        render json: { message: "Password has been reset successfully." }, status: :ok
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
     else
-      render :edit
+      render json: { error: "Password token is invalid or has expired" }, status: :not_found
     end
   end
 end
