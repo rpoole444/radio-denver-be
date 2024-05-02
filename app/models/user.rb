cdaeb7c0cb713568b3970e4854aa2fad4d4bede6
Nathan Lambertson <lambertones@gmail.com>
2024-05-02 10:17:35 -0500

    Alter User table to add description, password reset, host name, phone number and profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :phone_number, length: { minimum: 10, maximum: 15 }, allow_blank: true
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
   has_many :playlists, dependent: :destroy
