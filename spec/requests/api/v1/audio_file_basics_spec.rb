f1af0b34f1a649adf8d75c9c94ab7f1c0e83c472
Nathan Lambertson <lambertones@gmail.com>
2024-05-16 10:37:45 -0500

    Add test for update audio file belonging to a user

--- spec/requests/api/v1/audio_file_basics_spec.rb
+++ spec/requests/api/v1/audio_file_basics_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe "Files requests", type: :request do
     end
   end
 
-  describe "GET /users/:user_id/audio_files/:id" do
+  describe "GET /users/:user_id/audio_files/:id", type: :request do
     it "returns a single file belonging to a user" do
       user = User.create!(first_name: "John", last_name: "Doe", email: "lamb@gmail.com", password: "1234password", password_confirmation: "1234password")
       audio_file1 = user.audio_files.create!(name: "file1", size: 100, s3_key: "key1")
@@ -61,6 +61,25 @@ RSpec.describe "Files requests", type: :request do
       expect(assigns(:audio_file)).to be_a(AudioFile)
       expect(assigns(:audio_file)).to be_persisted
       expect(assigns(:audio_file).id).to eq(audio_file1.id)
+    end
+  end
+
+  describe "PUT /users/:user_id/audio_files/:id", type: :request do
+    it "updates a file belonging to a user" do
+      user = User.create!(first_name: "John", last_name: "Doe", email: "lamb@gmail.com", password: "1234password", password_confirmation: "1234password")
+      audio_file1 = user.audio_files.create!(name: "file1", size: 100, s3_key: "key1")
+      headers = { 'Authorization' => "Bearer #{JsonWebTokenService.encode(user_id: user.id)}" }
+
+      put "/api/v1/users/#{user.id}/audio_files/#{audio_file1.id}", params: { audio_file: { name: "new_name", size: 333, s3_key: "new_key" } }, headers: headers
+
+      expect(response).to have_http_status(:ok)
+
+      json_response = JSON.parse(response.body)
+
+      expect(json_response["name"]).to eq("new_name")
+      expect(json_response["size"]).to eq(333)
+      expect(json_response["s3_key"]).to eq("new_key")
+      expect(assigns(:audio_file)).to eq(audio_file1)
     end
   end
 end
