a0c4c4a50aadc8f7aa4061b6703383337eb1c04d
Nathan Lambertson <lambertones@gmail.com>
2024-05-16 09:15:08 -0500

    Commit for branch change

--- app/controllers/api/v1/audio_files_controller.rb
+++ app/controllers/api/v1/audio_files_controller.rb
@@ -1,10 +1,10 @@
-class AudioFilesController < ApplicationController
+class Api::V1::AudioFilesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_audio_file, only: [:show, :update, :destroy]
 
   # GET /audio_files
   def index
     @audio_files = current_user.audio_files
-
     render json: @audio_files
   end
 
@@ -18,9 +18,9 @@ class AudioFilesController < ApplicationController
     @audio_file = current_user.audio_files.build(audio_file_params)
 
     if @audio_file.save
-      render json: @audio_file, status: :created, location: @audio_file
+      render json: @audio_file, status: :created
     else
-      render json: @audio_file.errors, status: :unprocessable_entity
+      render json: { errors: @audio_file.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
@@ -29,23 +29,23 @@ class AudioFilesController < ApplicationController
     if @audio_file.update(audio_file_params)
       render json: @audio_file
     else
-      render json: @audio_file.errors, status: :unprocessable_entity
+      render json: { errors: @audio_file.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   # DELETE /audio_files/1
   def destroy
     @audio_file.destroy
+    head :no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_audio_file
-      @audio_file = current_user.audio_files.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def audio_file_params
-      params.require(:audio_file).permit(:name, :size, :s3_key)
-    end
+  def set_audio_file
+    @audio_file = current_user.audio_files.find(params[:id])
+  end
+
+  def audio_file_params
+    params.require(:audio_file).permit(:name, :size, :s3_key)
+  end
 end
--- spec/requests/api/v1/audio_file_basics_spec.rb
+++ spec/requests/api/v1/audio_file_basics_spec.rb
@@ -1,56 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Files requests", type: :request do
-  xdescribe "GET index" do
-    it "returns a list of files" do
-      user = User.create!(first_name: "John", last_name: "Doe", email: "lamb@gmail.com", password: "1234password", password_confirmation: "1234password")
-      file1 = user.audio_files.create!(name: "file1", size: 100, s3_key: "key1")
-      file2 = user.audio_files.create!(name: "file2", size: 200, s3_key: "key2")
-
-      get :index, params: { user_id: user.id }
-
-      expect(response).to have_http_status(:success)
-      expect(assigns(:audio_files)).to match_array([file1, file2])
-    end
-  end
-
-  xdescribe "GET show" do
-    it "returns a single file" do
-      user = User.create!(first_name: "John", last_name: "Doe", email: "lamb@gmail.com", password: "1234password", password_confirmation: "1234password")
-      file = user.audio_files.create!(name: "file1", size: 100, s3_key: "key1")
-
-      get :show, params: { user_id: user.id, id: file.id }
-
-      expect(response).to have_http_status(:success)
-      expect(assigns(:audio_file)).to eq(file)
-    end
-  end
-
-  describe "POST create" do
+  describe "POST /users/:user_id/audio_files" do
     it "creates a new file" do
       user = User.create!(first_name: "John", last_name: "Doe", email: "lamb@gmail.com", password: "1234password", password_confirmation: "1234password")
       file_params = { name: "new_file", size: 300, s3_key: "new_key" }
 
       expect {
-        post :create, params: { user_id: user.id, audio_file: file_params }
+        post "/api/v1/users/#{user.id}/audio_files", params: { audio_file: file_params }
       }.to change(AudioFile, :count).by(1)
 
       expect(response).to have_http_status(:created)
       expect(assigns(:audio_file)).to be_a(AudioFile)
       expect(assigns(:audio_file)).to be_persisted
-    end
-  end
-
-  xdescribe "DELETE destroy" do
-    it "deletes a file" do
-      user = User.create!(first_name: "John", last_name: "Doe", email: "lamb@gmail.com", password: "1234password", password_confirmation: "1234password")
-      file = user.audio_files.create!(name: "file1", size: 100, s3_key: "key1")
-
-      expect {
-        delete :destroy, params: { user_id: user.id, id: file.id }
-      }.to change(AudioFile, :count).by(-1)
-
-      expect(response).to have_http_status(:no_content)
     end
   end
 end
